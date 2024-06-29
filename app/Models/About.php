e4cce86cf38fab88110e71ed2ae8ad30cbc3d3a7
hossam hassan <hossamhassan889@gmail.com>
2024-06-29 20:15:19 +0300

    added about

--- app/Models/About.php
+++ app/Models/About.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class About extends Model
 {
-    use HasFactory;
+    protected $table = 'about';
+
+    protected $fillable = [
+        'ar_name',
+         'en_name',
+          'support_number',
+          'support_email'   
+    ];
 }
