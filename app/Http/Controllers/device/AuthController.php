814b6ae41b41b08f9b57d7cd3558b7238e1afc84
hossam hassan <hossamhassan889@gmail.com>
2024-06-29 20:23:58 +0300

    aerrrr

--- app/Http/Controllers/device/AuthController.php
+++ app/Http/Controllers/device/AuthController.php
@@ -190,13 +190,17 @@ class AuthController extends Controller
     public function getInfoAbout(){
 
          $about = About::where('id', '=', 1)->first();
-        return response()->json( [
+
+
+       $aboutt =   [
             'ar_name'=> $about->ar_name,
             'en_name'=>  $about->en_name,
             'support_number'=>   $about->support_number,
             'support_email'=> $about->support_email,
             
-        ] , 200); 
+              ];
+
+        return response()->json( [ 'about'=> $aboutt]  , 200); 
 
     }
 
